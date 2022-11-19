5255eea8ac6750562e0dff07f0af7502572046dd
omar3o3 <otantawy53@gmail.com>
2022-11-19 18:08:27 -0500

    removed customBlogSerializer file and added it to the blog serializer file, and deleted commented out code

--- blogs/serializers.py
+++ blogs/serializers.py
@@ -6,14 +6,9 @@ from tagBlogs.serializers import TagBlogSerializer
 from tagBlogs.serializers import CustomTagBlogSerializer
 
 class BlogSerializer(serializers.ModelSerializer):
-    # user = serializers.SlugRelatedField(read_only=True, slug_field='user_name')
-    # tagblog_set = serializers.StringRelatedField(many=True)
-
     class Meta:
         model = Blog
         fields = ['title', 'content', 'id', 'user']
-        # fields = ['title', 'content', 'id', 'user', 'tagblog_set']
-        
         
 class CustomBlogSerializer(serializers.ModelSerializer):
     user = serializers.SlugRelatedField(read_only=True, slug_field='user_name')
