272567d0acaaa0c3c8fe42509ee2d4214823c763
omar3o3 <otantawy53@gmail.com>
2022-11-20 16:59:16 -0500

    added str method for comment class

--- comments/models.py
+++ comments/models.py
@@ -7,3 +7,7 @@ class Comment(models.Model):
     user = models.ForeignKey(NewUser, on_delete = models.CASCADE)
     content =  models.CharField(max_length = 500)
     blog = models.ForeignKey(Blog, on_delete = models.CASCADE)
+    
+    
+    def __str__(self):
+        return f"id: {self.id}, blog: {self.blog.id}, user: {self.user.id}"
