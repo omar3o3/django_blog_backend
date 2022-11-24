167256ee7aabc5a5c90b8c0024f8108454519202
omar3o3 <otantawy53@gmail.com>
2022-11-23 21:32:48 -0500

    changes url path to get all users

--- users/urls.py
+++ users/urls.py
@@ -11,7 +11,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('create_user', views.create_user),
     path('test_run', views.test_run),
-    path('get_users', views.get_users),
+    path('get-users', views.get_users),
     # path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
